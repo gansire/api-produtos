bbffc1423c6994c553c785a0cc5b4f204002296a
gansire <jean16203@gmail.com>
2025-04-25 21:50:49 -0300

    criado endpoint do produto

---
+++ prisma/migrations/20250426002711_add_category_relation/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "parentId" TEXT,
+    CONSTRAINT "Category_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Category" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "qty" INTEGER NOT NULL,
+    "price" REAL NOT NULL,
+    "photo" TEXT NOT NULL,
+    "categoryId" TEXT NOT NULL,
+    CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -5,23 +5,48 @@ import { PrismaService } from '../../prisma/prisma.service';
 export class ProductService {
   constructor(private prisma: PrismaService) {}
 
-  findAll(){
-
+  findAll(name?: string){
+    return this.prisma.product.findMany({
+      where: name ? { name: { contains: name.toLowerCase() } } : undefined,
+      include:{ category : true}
+    })
   }
 
-  findOne(){
-
+  findOne(id: string){
+    return this.prisma.product.findUnique({
+      where: { id },
+      include: { category: true },
+    });
   }
 
-  create(){
-
+  create(body: any) {
+    return this.prisma.product.create({
+      data: {
+        name: body.name,
+        qty: body.qty,
+        price: body.price,
+        photo: body.photo,
+        categoryId: body.categoryId,
+      },
+      include: { category: true },
+    });
   }
 
-  update(){
-
+  update(id: string, body: any) {
+    return this.prisma.product.update({
+      where: { id },
+      data: {
+        ...body,
+        categories: body.categories ? 
+          {
+            set: body.categories.map((id: string) => ({ id })),
+          }
+          : undefined,
+      },
+    });
   }
 
-  delete(){
-    
+  delete(id: string){
+    return this.prisma.product.delete({where: {id}})
   }
 }
